e8d3da25b0e3cf76622b55daaa98f305fab1190e
Z <33862677@qq.com>
2021-06-11 09:50:41 +0800

    add ignore file

---
+++ masterEth/sol/contracts/risk/over-underflow/TimeLock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <=0.8.4;
+
+contract TimeLock {
+    mapping(address => uint256) public balance;
+    mapping(address => uint256) public locktime;
+
+    function deposit() public payable {
+        balance[msg.sender] += msg.value;
+        locktime[msg.sender] = block.timestamp + 1 weeks;
+    }
+
+    function increamLocktime(uint256 _secondToIncrease) public {
+        locktime[msg.sender] += _secondToIncrease; //uint256 进行增加操作, 可能存在范围溢出
+    }
+
+    function withdraw() public {
+        require(balance[msg.sender] > 0);
+        require(block.timestamp > locktime[msg.sender]); //当出现locktime溢出后, 此条件始终是 true
+        balance[msg.sender] = 0;
+        payable(msg.sender).transfer(balance[msg.sender]);
+    }
+}
