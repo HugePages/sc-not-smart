39caf3377ad4c191287d629593170bb0df931a3d
Z <33862677@qq.com>
2021-06-15 23:56:26 +0800

    40000

--- masterEth/sol/contracts/risk/unexcept-ether/EtherGame.sol
+++ masterEth/sol/contracts/risk/unexcept-ether/EtherGame.sol
@@ -15,5 +15,32 @@ contract EtherGame {
     //满足条件三的奖励金额
     uint256 public milStone3Rewards = 10 ether;
 
+    uint256 public balanceTmp = 0;
     mapping(address => uint256) redeemableEther;
+
+    function play() public payable {
+        require(msg.value == 0.5 ether);
+        uint256 currentBalance = balanceTmp + msg.value;
+
+        require(currentBalance <= payoutMilestone3);
+
+        if (currentBalance == payoutMilestone1) {
+            redeemableEther[msg.sender] += milStone1Rewards;
+        }
+        if (currentBalance == payoutMilestone2) {
+            redeemableEther[msg.sender] += milStone2Rewards;
+        }
+        if (currentBalance == payoutMilestone3) {
+            redeemableEther[msg.sender] += milStone3Rewards;
+        }
+        balanceTmp += msg.value;
+        return;
+    }
+
+    function claimReward() public {
+        require(balanceTmp == milStone3Rewards);
+        require(redeemableEther[msg.sender] > 0);
+        redeemableEther[msg.sender] = 0;
+        payable(msg.sender).transfer(milStone3Rewards);
+    }
 }
